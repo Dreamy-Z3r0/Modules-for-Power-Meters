0debb0374e1b13c556b947444832ff4689431a05
z3r0-0112 <34434717+z3r0-0112@users.noreply.github.com>
2019-07-16 09:33:54 +0700

    Create PROCESS.h

---
+++ DRIVER/PROCESS/PROCESS.h
@@ -0,0 +1,21 @@
+/*
+ * PROCESS.h
+ *
+ *  Created on: July 15, 2019
+ *      Author: Z3r0
+ */
+
+#ifndef DRIVER_PROCESS_PROCESS_H_
+#define DRIVER_PROCESS_PROCESS_H_
+
+#include <msp430.h>
+#include <stdint.h>
+#include <math.h>
+
+#include "GPIO/GPIO.h"
+#include "TIMER/TIMER.h"
+#include "UART/UART.h"
+
+
+
+#endif /* DRIVER_PROCESS_PROCESS_H_ */
