91c73ba8420f9ea241f1dafb01085111dc30e018
thiphuong <phuongnguyen.30070@gmail.com>
2019-07-17 11:04:18 +0700

    Update TIMER.h

--- DRIVER/TIMER/TIMER.h
+++ DRIVER/TIMER/TIMER.h
@@ -1,1 +1,13 @@
+#ifndef DRIVER_TIMER_TIMER_H_
+#define DRIVER_TIMER_TIMER_H_
+
+#include "DRIVER/PROCESS/PROCESS.h"
+
+
+void TIMER_init(void);
+void TIMER_0_init(void);
+void set_SMCLK(void);
+
+
+#endif /* DRIVER_TIMER_TIMER_H_ */
 
