0566abfca14ddf9ff466ef2aa8ffd7380f29fc72
thiphuong <phuongnguyen.30070@gmail.com>
2019-07-19 11:01:30 +0700

    Update Flash.h

--- DRIVER/Flash/Flash.h
+++ DRIVER/Flash/Flash.h
@@ -1,1 +1,18 @@
+/*
+ * FLASH.h
+ *
+ *  Created on: Jul 19, 2019
+ *      Author: eeit2
+ */
+
+#ifndef DRIVER_FLASH_FLASH_H_
+#define DRIVER_FLASH_FLASH_H_
+
+#include "DRIVER/PROCESS/PROCESS.h"
+
+void Flash_init(void);
+void write_segA(char write_result, uint8_t write_size);
+void read_segA(char read_result, uint8_t read_size);
+
+#endif /* DRIVER_FLASH_FLASH_H_ */
 
