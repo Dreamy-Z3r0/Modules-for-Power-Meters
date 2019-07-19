51708840d1a7b43cb2b27ac7faca70291d76be75
z3r0-0112 <34434717+z3r0-0112@users.noreply.github.com>
2019-07-19 14:54:12 +0700

    Add files via upload

--- DRIVER/UART/UART.c
+++ DRIVER/UART/UART.c
@@ -1,81 +1,112 @@
-#include "msp430.h"
-#include <stdint.h>
-#include <math.h>
+/*
+ * UART.c
+ *
+ *  Created on: 9 thg 1, 2019
+ *      Author: Administrator
+ */
 
+#include "UART.h"
 
+void UART_init()
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-void UART_init(void){
-
-    // Enable UART Connection
-    P1SEL0 |=   BIT2|BIT3;
-    P1SEL1 &= ~(BIT2|BIT3);
+    P1SEL1 &= ~(BIT2 | BIT3); // configure P1.2 and P1.3 for UART communication (RXD and TXD pins, respectively)
+    P1SEL0 |= BIT2 | BIT3;
 
     // UART specific configuration
     UCA0CTL1 |= UCSWRST;    // reset state enabling
     UCA0CTL1 |= UCSSEL_2;   // choose SMCLK = 16.384MHz as UART clock source
-
-    UCA0BR0   = 142;                    // 115200 baud, cai dat clock baud rate
-    UCA0BR1   = 0;
-    UCA0MCTLW = 0x2200;                 // 16.384MHz/115200 = 142.22 (See UG)
-
+    UCA0BR1 = 0;            // configure baud rate to 115200
+    UCA0BR0 = 142;
+    UCA0MCTLW = 0x2200;     // UCBRS0 = 0x22 and oversampling mode is disabled
     UCA0CTL1 &= ~UCSWRST;    // disable reset state
 
-    // Enable UART interrupt
+    // enable UART interrupt
     UCA0IE |= UCTXIE;
     UCA0IE |= UCRXIE;
 }
 
-void UART_write_buff(uint8_t *buff, uint8_t len){
-    uint8_t i;
-    for(i = 0; i < len; i++)
+void UART_write_buff(uint8_t *buff)
+{
+    while (UCA0STATW & UCBUSY)
+        ;
+    while (!(UCA0IFG & UCTXIFG))
+        ;
+    UCA0TXBUF = *buff;
+}
+
+void UART_input_String(uint8_t *buff, uint8_t buff_length)
+{
+    int index;
+    for (index = 0; index < buff_length; index++)
     {
-        while(UCA0STATW & UCBUSY);
-        while (!(UCA0IFG & UCTXIFG));   // USCI_A0 TX buffer ready?
-        UCA0TXBUF = buff[i];
+        UART_write_buff(buff);
+        buff++;
     }
 }
 
-void write_string(uint8_t *str){
-    while(*str != '\0'){
-        UART_write_buff(str,1);
-        str++;
+void UART_input_number(float number)
+{
+    int flag = 0;   // if number < 0, flag = 1
+    if (0 > number)
+        flag = 1;
+
+    /* Get string length */
+    int power = 1;  // Length of unsigned number
+    while (0 != (unsigned int) ((number * 100) / pow(10, power)))
+    {
+        power++;
     }
+    flag = flag + power;        // Length of the signed number
+
+    /* Convert number to string */
+    // It is agreed that the sender sends a number multiplied by 100 and neglects the floating point.
+    uint8_t *string = (uint8_t*) malloc(flag * sizeof(uint8_t));
+    uint8_t *location = string;
+    uint8_t *location_free = string;
+
+    if (0 > number)
+    {
+        number = -number;   // acquires unsigned number
+        *location = '-';
+        location++;
+    }
+
+    itoa((unsigned int) (number * 100), power, location, 10);
+
+    /* Sends converted number via UART */
+    UART_input_String(string, flag);
+    free(location_free);
 }
 
-void write_num(float x){
-    long num = x*100, isNegative = 0;
-    if(num < 0){
-        isNegative = 1;
-        num = -num;
-    }
-
-
-
-
-    uint8_t str[100];
-    uint8_t i = 0;
-
-    // Convert integer to string
-    while(num != 0)
+void itoa(unsigned int number, int number_length, uint8_t *string, int base)
+{
+    if (10 == base)     // Number is desired to be sent as decimal number
     {
-        uint8_t rem = num%10; // obtain the last digit
-        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0'; // convert the number to ASCII
-        num = num/10;
+        int order = number_length - 1;   // used for acquiring most left number character
+
+        int x,
+            max = 10;   // Desired max is 10^order
+        if (0 == order)
+            max = 1;    // max = 10^0 = 1
+        else
+        {
+            for (x = 1; x < order; x++)     // max = 10^order
+            {
+                max = max * 10;
+            }
+        }
+
+        int tmp_num;
+        while (0 < number_length)     // next character 'till the end of string (index = 0 for positive number, and index = 1 for negative number)
+        {
+            tmp_num = number / max;         // acquires left most number character of unsigned input number
+            number = number - tmp_num * max;    // remove left most character of unsigned input number
+            *string = tmp_num + '0';            // converts left most number to character
+            max = max / 10;
+            string++;
+            number_length--;
+        }
     }
-    if(isNegative) str[i++] = '-';
-    str[i] = '\0';      // end the string
-
-    // Reverse the string
-    uint8_t final[100];
-    uint8_t begin, end, count = 0;
-    while(str[count] != '\0') count++;
-    end = count - 1;
-
-    for(begin = 0; begin < count; begin++){
-        final[begin] = str[end];
-        end--;
-    }
-    final[begin] = '\0';    // end the string
-
-    write_string(final);
 }
--- DRIVER/UART/UART.h
+++ DRIVER/UART/UART.h
@@ -1,19 +1,22 @@
 /*
  * UART.h
  *
- *  Created on: Jul 15, 2019
- *      Author: USER
+ *  Created on: 9 thg 1, 2019
+ *      Author: Administrator
  */
 
-#include "msp430.h"
+#include <msp430.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
 
 #ifndef DRIVER_UART_UART_H_
 #define DRIVER_UART_UART_H_
 
-void UART_init(void);
-void UART_write_buff(uint8_t *buff, uint8_t len);
-void write_string(uint8_t *str);  // Write the string to the buffer
-void write_num(float x);   // Converts an integer into string and writes it to the buffer
+void UART_init();
+void UART_write_buff(uint8_t *buff);
+void UART_input_String(uint8_t *buff, uint8_t buff_length);
+void UART_input_number(float number_in);
+void itoa(unsigned int number, int number_length, uint8_t *string, int base);
 
 #endif /* DRIVER_UART_UART_H_ */
