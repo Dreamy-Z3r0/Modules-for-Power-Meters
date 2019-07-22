34ec39722e70ac052c70fe8c194a870e9c2e9238
thiphuong <phuongnguyen.30070@gmail.com>
2019-07-22 09:44:02 +0700

    Update Flash.h

--- DRIVER/Flash/Flash.h
+++ DRIVER/Flash/Flash.h
@@ -5,14 +5,35 @@
  *      Author: eeit2
  */
 
+#include "DRIVER/PROCESS/PROCESS.h"
+
+
 #ifndef DRIVER_FLASH_FLASH_H_
 #define DRIVER_FLASH_FLASH_H_
 
-#include "DRIVER/PROCESS/PROCESS.h"
+#define FLASH_SIZE 24
+#define FLASH_ADDR_SETTING        0xEC00
+#define FLAH_ADDR_TEMP            0xF000
+
+
+typedef struct
+{
+uint8_t temperature[FLASH_SIZE];
+}FLASH_typedef;
+
+extern FLASH_typedef FLASH;
+
 
 void Flash_init(void);
+
+void setting_infor(void);
+void update_infor(void);
+
 void write_segA(char write_result, uint8_t write_size);
 void read_segA(char read_result, uint8_t read_size);
 
-#endif /* DRIVER_FLASH_FLASH_H_ */
 
+void store_result(uint16_t temperature_addr);
+void update_result(uint16_t temperature_addr);
+
+#endif /* DRIVER_FLASH_FLASH_H_ */
