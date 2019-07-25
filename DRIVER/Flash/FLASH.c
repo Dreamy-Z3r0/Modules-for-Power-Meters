d2522e7d9c0878633799da2703ef4c8e6daeeb38
ERROR-314 <52988448+ERROR-314@users.noreply.github.com>
2019-07-25 14:15:09 +0700

    Add files via upload

--- DRIVER/Flash/FLASH.c
+++ DRIVER/Flash/FLASH.c
@@ -4,7 +4,7 @@
  *  Created on: Jul 19, 2019
  *      Author: eeit2
  *
- *  Updated on: July 23, 2019
+ *  Updated on: July 25, 2019
  *      Author: thebeztpersonthateverlivedinthisworldinpastpresentandfutureandthereisnothingyoucansayaboutitthatcanchangetheveryfactofit.Mymereexistencedisproveswhateveryouhavetosayaboutit:P
  *
  *  Description:
@@ -12,15 +12,25 @@
  *      Related information are placed in the same 1KB segments for better
  *      data handling. (e.g. when the whole segment of temperature needs to
  *      be overwritten, only that specific segment needs to be erased,
- *      and other data is not touched)
+ *      and other data is not touched) This is done due to the fact that the
+ *      smallest amount of flash that can be erased is one segment.
+ *
+ *      Note:   The flash write is not yet optimized because of two reasons.
+ *              1) every time a piece of data is written to the FLASH memory,
+ *              the CPU is held and everything else stops.
+ *              2) the whole write operation switches on and off every
+ *              character, as opposed to being switched on for the whole
+ *              write operation.
+ *
  *
  *  Software description:
  *      FLASH_init(..) : initiate FLASH function
  *      FLASH_write_seg(..) : write a value to a segment in the FLASH Memory
  *      FLASH_read_seg(..) : reads a value from a segment in the FLASH Memory
  *      FLASH_erase_seg(..) : erases a whole segment in the FLASH Memory
+ *      FLASH_update_temp(..) : updates value temperature
  */
-#include "DRIVER/FLASH/FLASH.h"
+#include "../FLASH/FLASH.h"
 
 void FLASH_init(void)
 {
@@ -37,42 +47,52 @@ void FLASH_init(void)
     FCTL2 = FWKEY | FSSEL_2 | FN3 | FN5;
 }
 
-void FLASH_write_seg(uint16_t write_addr, uint8_t *value, uint8_t write_length)
+void FLASH_update_temp(void)
 {
+    /* Place read temperature values from SD24 into corresponding positions in array
+     * temp_values[0] = Kelvin Temperature  // Address: 0xF800
+     * temp_values[1] = Reference Voltage   // Address: 0xF802
+     * temp_values[2] = TC Sensor           // Address: 0xF804
+     * temp_values[3] = Offset Voltage      // Address: 0xF806
+     */
+    uint16_t temp_values[4] = {0, 0, 0, 0};
+
+    FLASH_write_seg(TEMP_K, temp_values, 4);
+}
+
+void FLASH_write_seg(uint16_t write_addr, uint16_t *value, uint8_t write_length)
+{
+
+    FLASH_erase_seg(write_addr);
     uint16_t *Flash_ptr;                    // Flash pointer
     Flash_ptr = (uint16_t *)write_addr;     // Initialize Flash pointer
 
-    /*
-     Clear Flash
-    FCTL1 = FWKEY | ERASE;
-    while(FCTL3 & BUSY);
-
-    if(FCTL3 & LOCKSEG) {                   // If Info Seg is still locked
+    if(FCTL3 & LOCKSEG)                     // If Info Seg is still locked
+    {
         FCTL3 = FWKEY | LOCKSEG;            // Clear LOCKSEG bit
     }
-     */
-    FCTL3 = FWKEY;                          // Clear LOCK, to unlock flash memory for flash writing
-    FCTL1 = FWKEY | WRT;                    // Set WRT, to enable Write operation
 
+    FCTL1 = FWKEY | WRT;                    // Set WRT, to enable Write operation
     while(write_length--)
     {
-        *Flash_ptr++ = *value++;            // Write value to flash
+        *Flash_ptr++ = (*value++);          // Write value to flash
         while(FCTL3 & BUSY);
     }
 
     FCTL1 = FWKEY;                          // Clear WRT, to disable Write operation
-    FCTL3 = FWKEY | LOCK;                   // Set LOCK
+    FCTL3 = FWKEY | LOCK | LOCKSEG;         // Set LOCK
 
 }
 
-void FLASH_read_seg(uint16_t read_addr, uint8_t *value ,uint8_t read_length)
+void FLASH_read_seg(uint16_t read_addr, uint16_t *value, uint8_t read_length)
 {
+
     uint16_t *Flash_ptr;                    // Flash pointer
     Flash_ptr = (uint16_t *)read_addr;      // Initialize Flash pointer
 
     while(read_length--)
     {
-       *value++ = *Flash_ptr++;             // Store flash to value
+        (*value++) = *Flash_ptr++;          // Store flash to value
     }
 }
 
@@ -80,13 +100,16 @@ void FLASH_erase_seg(uint16_t erase_addr)
 {
     uint16_t *Flash_ptr;                    // Flash pointer
     Flash_ptr = (uint16_t *)erase_addr;     // Initialize Flash pointer
-    FCTL3 = FWKEY;                          // Clear LOCK, to unlock flash memory erase operation
+    if(FCTL3 & LOCKSEG)                     // If Info Seg is stil locked
+    {
+        FCTL3 = FWKEY | LOCKSEG;            // Clear LOCKSEG bit
+    }
     FCTL1 = FWKEY | ERASE;                  // Set ERASE, to enable Segment Erase operation
 
     *Flash_ptr = 0;                         // Dummy Write, to initiate Erase operation
     while(FCTL3 & BUSY);
 
     FCTL1 = FWKEY;                          // Clear ERASE, to disable Segment Erase operation
-    FCTL3 = FWKEY | LOCK;                   // Set LOCK
+    FCTL3 = FWKEY | LOCK | LOCKSEG;                   // Set LOCK
 
 }
--- DRIVER/Flash/FLASH.h
+++ DRIVER/Flash/FLASH.h
@@ -13,14 +13,22 @@
 #define DRIVER_FLASH_FLASH_H_
 
 // Temperature Info
-#define TEMP_ADDR 0x1060
-#define TEMP_VREF       TEMP_ADDR + 8       // Reference Voltage
-#define TEMP_TCSENSOR   TEMP_VREF + 4       //
-#define V_OFFSET        TEMP_TCSENSOR + 4   // Offset Voltage
+/*
+ * 1 address points to an 8 bit value
+ * To store a 16 bit value, 2 addresses are required
+ *
+ */
+#define TEMP_K          0xF800
+#define TEMP_VREF       TEMP_K + 2          // Reference Voltage
+#define TEMP_TCSENSOR   TEMP_VREF + 2       //
+#define TEMP_VOFFSET    TEMP_TCSENSOR + 2   // Offset Voltage
+
+
 
 void FLASH_init(void);
-void FLASH_write_seg(uint16_t write_addr, uint8_t *value, uint8_t write_length);
-void FLASH_read_seg(uint16_t read_addr, uint8_t *value ,uint8_t read_length);
+void FLASH_write_seg(uint16_t write_addr, uint16_t *value, uint8_t write_length);
+void FLASH_read_seg(uint16_t read_addr, uint16_t *value, uint8_t read_length);
 void FLASH_erase_seg(uint16_t erase_addr);
+void FLASH_update_temp(void);
 
 #endif /* DRIVER_FLASH_FLASH_H_ */
